e1aba426ecddf872845d7182a0667ac8c9851281
Rakesh <chipparakesh01@gmail.com>
2024-04-14 20:03:26 +0530

    update.. to deploy

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
-from flask import Flask
+from datetime import datetime
+from pymongo import MongoClient
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
+current_datetime = datetime.now()
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+MONGODB_URL= os.getenv("MONGODB_URL")
 
-# if __name__ == '__main__':
+client = MongoClient(MONGODB_URL)
+
+db = client.get_database('stocks-market')
+
+
+
+# collection1 = db.get_collection(current_datetime.strftime("%Y_%m_%d"))
+# documents = collection1.find()
+# for document in documents:
+#     print(document)
+
+@app.route("/getData")
+def fetchData():
+    projection = {'_id': False}
+
+    getDate = request.args.get('date')
+    collectionName=current_datetime.strftime("%Y-%m-%d")
+
+    if getDate:
+        collectionName=getDate
+
+    # collection = db.get_collection(current_datetime.strftime("%Y_%m_%d"))
+    collection = db.get_collection(collectionName)
+
+    res = list(collection.find({},projection))
+    # client.close()
+    return jsonify(res)
+
+# if __name__ == "__main__":
 #     app.run()
+   
